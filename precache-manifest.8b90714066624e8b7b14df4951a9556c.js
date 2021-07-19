4e74da3d07beb5a365a406bf70e14f82e5b70a14
Aman Rathore <aman986838@gmail.com>
2021-07-19 15:59:47 +0530

    Updates

---
+++ precache-manifest.8b90714066624e8b7b14df4951a9556c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07d10445106a020164a422923f36eb5",
+    "url": "/MyNotepad/index.html"
+  },
+  {
+    "revision": "8245f0d67fab59b63371",
+    "url": "/MyNotepad/static/js/2.606b1192.chunk.js"
+  },
+  {
+    "revision": "413c413cde202d467d17",
+    "url": "/MyNotepad/static/js/main.d1a56c23.chunk.js"
+  },
+  {
+    "revision": "bd9b5baacae18f4ead34",
+    "url": "/MyNotepad/static/js/runtime-main.526427ce.js"
+  }
+]);
