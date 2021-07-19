80158f45cf5c1118d4154f64ace5de876f6e129b
Aman Rathore <aman986838@gmail.com>
2021-07-19 15:46:56 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        My-Notepad
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
